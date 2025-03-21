cdf307be9c4f855073ed1556ca1fa5c08393b3d1
Artur0campos <arturcamposcamara@gmail.com>
2025-03-20 22:10:50 -0300

    colocando temporizador

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -1,24 +1,39 @@
-//
-//  ViewController.swift
-//  EggTimer
-//
-//  Created by Angela Yu on 08/07/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
     
-    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12  ]
-
-    @IBAction func hardnessSelected(_ sender: UIButton) {
-        let hardness = sender.currentTitle
-        
-        print(eggTimes[hardness!] ?? "Error")
-            
-        }
-}
     
+    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
+    
+    var seconds = 60
+    var timer: Timer?
+    
+    @IBOutlet weak var timerLabel: UILabel!
+    
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        let hardness = sender.currentTitle!
+        
+        print(eggTimes[hardness] ??  "Error")
+        
+        seconds = eggTimes[hardness] ?? 60
+        timerLabel.text = "\(seconds) seconds"
+        
+        timer?.invalidate()
+        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateCounter() {
 
-
+        if seconds > 0 {
+            timerLabel.text = "\(seconds) seconds"
+            seconds -= 1
+        }
+        
+        else {
+            timer?.invalidate()
+            timerLabel.text = "Done!"
+        }
+    }
+    
+}
