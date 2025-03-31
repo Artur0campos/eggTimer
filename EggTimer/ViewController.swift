c5d0547e11111f1f33ce6c15c4f39bc7951723a1
Artur0campos <arturcamposcamara@gmail.com>
2025-03-31 20:59:25 -0300

    corrigindo bug e finalizando a barra de progresso

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -4,8 +4,8 @@ class ViewController: UIViewController {
     
     let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
     
-    var seconds = 0
-    var totalTime = 0
+    var seconds: Float = 0
+    var totalTime: Float = 0
     var timer = Timer()
     
     @IBOutlet weak var progressBar: UIProgressView!
@@ -13,29 +13,30 @@ class ViewController: UIViewController {
     @IBOutlet weak var upperLabel: UILabel!
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
+    
+        let hardness = sender.currentTitle!
         
         timer.invalidate()
-        
-        let hardness = sender.currentTitle!
-        print(eggTimes[hardness] ??  "Error")
-        
-        totalTime = eggTimes[hardness]!
-        
         progressBar.progress = 0.0
+        seconds = 0
+        upperLabel.text = hardness
         
-        upperLabel.text = "\(seconds) seconds"
+        totalTime = Float(eggTimes[hardness]!)
         
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
     }
     
     @objc func updateCounter() {
-
-        if seconds > totalTime {
-            upperLabel.text = "\(totalTime) seconds"
-            seconds += 1
+        var counter: Int = 0
+        if seconds < totalTime {
             
-            let percentegeProgress = Float(seconds / totalTime)
+            let percentegeProgress = seconds / totalTime
+            print(percentegeProgress)
             progressBar.progress = percentegeProgress
+            
+            counter = Int(totalTime - seconds)
+            upperLabel.text = "\(counter) seconds"
+            seconds += 1
         }
         
         else {
@@ -45,3 +46,5 @@ class ViewController: UIViewController {
     }
     
 }
+
+
